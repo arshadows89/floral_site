b3e00b0673b578e702dbb0e7bce2d74a1dbd903a
Cameron Snyder <Dooffycom@gmail.com>
2015-06-10 16:03:23 -0400

    working test masonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,33 @@
 //= require_tree .
 //= require jquery
 //= require bootstrap-sprockets
+//= require imagesloaded
+$(document).ready(function() {
+	(function( $ ) {
+
+	    var $container = $('.masonry-container');
+	    $container.imagesLoaded( function () {
+	        $container.masonry({
+	            columnWidth: '.item',
+	            itemSelector: '.item'
+	        });
+	    });
+	    
+	    //Reinitialize masonry inside each panel after the relative tab link is clicked - 
+	    $('a[data-toggle=tab]').each(function () {
+	        var $this = $(this);
+
+	        $this.on('shown.bs.tab', function () {
+	        
+	            $container.imagesLoaded( function () {
+	                $container.masonry({
+	                    columnWidth: '.item',
+	                    itemSelector: '.item'
+	                });
+	            });
+
+	        }); //end shown
+	    });  //end each
+	    
+	})(jQuery);
+});
